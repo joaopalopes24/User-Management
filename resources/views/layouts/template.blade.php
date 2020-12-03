281551fe70f9b635e92b5d29c6928cb08d5059eb
João Pedro Lopes <joaopalopes24@gmail.com>
2020-12-03 12:31:30 -0300

    Permissão, Novas View e Controllers

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -62,10 +62,10 @@
                 </li>
                 <li class="user-footer">
                   <div class="pull-left">
-                    <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                    <a href="#" class="btn btn-default btn-flat">Dados</a>
                   </div>
                   <div class="pull-right">
-                    <a href="{{route('login.logout')}}" class="btn btn-default btn-flat">Logout</a>
+                    <a href="{{route('login.logout')}}" class="btn btn-danger btn-flat">Logout</a>
                   </div>
                 </li>
               </ul>
--- resources/views/profiles/profiles-index.blade.php
+++ resources/views/profiles/profiles-index.blade.php
@@ -7,13 +7,60 @@
             Perfis
         </h1>
         <ol class="breadcrumb">
-            <li><a href="{{route('home.index')}}"><i class="fa fa-dashboard"></i> Página Inicial</a></li>
+            <li><a href="{{ route('home.index') }}"><i class="fa fa-dashboard"></i> Página Inicial</a></li>
             <li class="active">Perfis</li>
             <li class="active">Status</li>
         </ol>
     </section>
     <section class="content">
-        Teste
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <button class="btn btn-success pull-right">Novo Perfil</button>
+                    </div>
+                    <div class="box-body">
+                        <div class="row col-sm-12">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>First Name</th>
+                                            <th>Last Name</th>
+                                            <th>Username</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Concluir</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
 <!-- Fim Conteúdo -->
