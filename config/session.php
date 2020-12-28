9648789d522b93f3ccb8666127bbe19ce71ded7d
João Pedro Lopes <joaopalopes24@gmail.com>
2020-12-28 11:17:44 -0300

    Alterações nas configurações da Sessão

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => 'database',
+    'driver' => env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => 60,
+    'lifetime' => env('SESSION_LIFETIME', 120),
 
     'expire_on_close' => false,
 
